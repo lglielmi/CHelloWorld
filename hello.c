6c59f0be9eed9c3e01abb623fde890f00423feee
lglielmi <37587346+lglielmi@users.noreply.github.com>
2018-03-24 22:17:47 -0300

    Agregado de archivos TP 0
    Hello.c y Output.txt
    readme fue creado junto al repositorio.

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : hello.c
+ Author      : Luciano Glielmi
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+int main(void){
+ printf("Hello, World!\n");
+}
